3173fd9fe605408237ea4a686b4e40b1887351a5
zemse <sohamzemse143@gmail.com>
2019-11-05 17:05:02 +0530

    use match and loop for guessing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::io::stdin;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    println!("This is a game of guessing!");
+
+    loop {
+        println!("\nPlease input your guess:");
+
+        let mut guess = String::new();
+
+        stdin().read_line(&mut guess)
+            .expect("Failed to load line");
+
+        /// @dev trim method on string eleminates white spaces begnining and end
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid number");
+                continue;
+            }
+        };
+
+        println!("Your guess is {}", guess);
+
+        let secret_number = rand::thread_rng().gen_range(1,5);
+        // println!("secret_number: {}", secret_number);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!\n");
+                break;
+            }
+        }
+    }
 }
